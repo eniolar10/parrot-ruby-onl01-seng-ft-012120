0b58a24e3499443f5f8fa8c6e51d729b65f4dffe
Ola Enol <eniolar10@gmail.com>
2020-01-29 19:20:00 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(name="Squawk!")
+def parrot
+  name="Squawk!"
   puts "Squawk!"
   return "Squawk!"
   "Squawk!"
